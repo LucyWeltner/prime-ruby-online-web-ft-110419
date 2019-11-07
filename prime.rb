af4ab732d59b986c7670cba86ba263becbaa0951
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:13:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  i = 2
+  while i < integer/2 do 
+    if integer%i == 0 
+      return false 
+    else 
+      i += 1 
+    end 
+  end 
+  return true 
+end
+  
+  # Add  code here!
